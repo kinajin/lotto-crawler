2fe2c03118288e5ad674db43c48f16e058a7fa2a
kinajin <kinajin22@gmail.com>
2024-05-17 12:49:39 +0900

    debug: data 초기화 부분 수정

--- app/database.py
+++ app/database.py
@@ -4,14 +4,16 @@ from sqlalchemy.orm import sessionmaker
 
 
 # AWS RDS PostgreSQL 데이터베이스
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@3.34.216.218:5432/lotto"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@3.34.216.218:5432/lotto"
 
 # 희경 로컬 PostgreSQL 데이터베이스
 # LOCAL_SQLALCHEMY_DATABASE_URL = "postgresql://lotto:password@localhost:5432/lotto_db"
 
 # Docker 컨테이너의 PostgreSQL 데이터베이스
-# DOCKER_SQLALCHEMY_DATABASE_URL = "postgresql://lotto:password@localhost:5433/lotto_db"
+DOCKER_SQLALCHEMY_DATABASE_URL = "postgresql://lotto:password@localhost:5433/lotto_db"
 
 
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(DOCKER_SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
